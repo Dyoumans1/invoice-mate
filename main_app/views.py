7f2a3dcbdbecdf02bd9a6dfd2d3e45598c8fccc0
Dyoumans <derrikyoumans416@gmail.com>
2025-04-08 13:45:32 -0400

    Added base html structure, admin, basic model,  and auth features

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 
 # Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+class Home(LoginView):
+    template_name = 'home.html'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('client-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+
+def about(request):
+    return render(request, 'about.html')
